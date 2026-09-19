8ee1ef8024a0eed939724685deff35fc7ab10c28
Brunão <brunobroiettisere@edu.unifil.br>
2026-09-18 23:00:26 -0300

    Arrumando imagem do Home Page

--- AdotaPetFront-React-TS-Vite/src/screens/home/HomePage.tsx
+++ AdotaPetFront-React-TS-Vite/src/screens/home/HomePage.tsx
@@ -70,7 +70,7 @@ export default function HomePage() {
         <div style={{ position: "relative", width: "100%", height: "100%" }}>
           <p style={styles.heroTextoAdote}>ADOTE</p>
           <img
-            src="/imagens/dogtransparente.png"
+            src="/imagens/dogTransparente.png"
             alt="Gato e Cachorro"
             style={styles.heroImagem}
           />
